88afd5164bfb60a0040f770b11cca38151fb5c56
Albert Molano <aa.molano@uniandes.edu.co>
2019-03-30 17:59:54 -0500

    Working on remote conection
    local hosted to: http://2d4fc86b.ngrok.io/

--- src/app/estados-web/estados-web.service.ts
+++ src/app/estados-web/estados-web.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EstadoWeb } from './estadoWeb';
 import { Observable } from 'rxjs';
+import { AppConstants } from '../appConstants'
 
-const API_URL = "http://localhost:8080/s1_sitios-api/api/";
+const API_URL = AppConstants.baseURL;
 const estadosWeb = 'estadosWeb';
 
 @Injectable({
