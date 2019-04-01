d68653a233fc70db7ecc45143d2b262222279f61
Albert Molano <aa.molano@uniandes.edu.co>
2019-04-01 16:38:39 -0500

    State Bar service

--- src/app/estados-web/estados-web.service.ts
+++ src/app/estados-web/estados-web.service.ts
@@ -14,8 +14,38 @@ export class EstadosWebService {
 
   constructor(private http: HttpClient) { }
 
-  getEstadosWeb() : Observable<EstadoWeb[]> {
-    return this.http.get<EstadoWeb[]>(API_URL + estadosWeb);
+  getEstadosWeb() : number[] {
+
+    let a = this.http.get<EstadoWeb[]>(API_URL + estadosWeb);
+    let b :EstadoWeb[];
+    a.subscribe(value => b = value);
+    let activos:number;
+    let inavtivos:number;
+    let falla:number;
+    let otros:number;
+    b.forEach(estado => {
+      switch (estado.estado) {
+        case 'ACTIVO':
+          activos++;
+          break;
+        case'INCATIVO':
+          inavtivos++;
+          break;
+        case 'ENFALLA':
+          falla++;
+          break;
+        case 'OTROS':
+          otros++;
+          break;
+      }
+    });
+    let totales= activos+inavtivos+falla+otros;
+    activos=activos/totales;
+    inavtivos=inavtivos/totales;
+    falla=falla/totales;
+    otros=otros/totales;
+    let response=[activos,inavtivos,falla,otros];
+    return response;
   }
 
   getEstadosSitio(sitio:number) : Observable<EstadoWeb[]>
@@ -27,4 +57,6 @@ export class EstadosWebService {
   {
     return this.http.get<EstadoWeb>(API_URL+`websites/${sitio}/states/last`);
   }
+
+
 }
