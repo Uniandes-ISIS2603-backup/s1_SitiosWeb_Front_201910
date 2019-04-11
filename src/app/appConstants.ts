fc133a36daf56588a17ea428284a9987e8b3d81e
Daniel Preciado <de.preciado@uniandes.edu.co>
2019-04-11 11:29:42 -0500

    maqueta interfaz crear estado

--- src/app/appConstants.ts
+++ src/app/appConstants.ts
@@ -2,6 +2,6 @@ export class AppConstants {
     public static get baseURL(): string {
         //IMPORTANT esta url se mantendra por los test y debe ser cambiada por localhost:8080 al momento de la entrega
         //http://70e251ba.ngrok.io host termporal
-        return "http://70e251ba.ngrok.io/s1_sitios-api/api/";
+        return "http://localhost:8080/s1_sitios-api/api/";
     }
 }
