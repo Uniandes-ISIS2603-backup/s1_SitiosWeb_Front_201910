aead3cd90939d00c1b76bdf43ee86c1746944db6
Julian Amezquita <js.amezquita@uniandes.edu.co>
2019-04-11 21:02:25 -0500

    hola soy german

--- src/app/technologies/technologies-detail/technologies-detail.component.ts
+++ src/app/technologies/technologies-detail/technologies-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { TechnologiesService } from '../technologies.service';
 import { ActivatedRoute } from '@angular/router';
 import { TechnologyDetail } from '../technologies-detail';
@@ -25,18 +25,17 @@ export class TechnologiesDetailComponent implements OnInit {
   /**
   * The tech's id retrieved from the address
   */
-  tech_id: number;
+ @Input() tech_id: number;
 
   getTechnologyDetail(): void {
     this.techService.getTechnologyDetail(this.tech_id)
-      .subscribe(adminDetail => {
-        this.techDetail = adminDetail
+      .subscribe(detail => {
+        this.techDetail = detail
       });
   }
 
 
   ngOnInit() {
-    this.tech_id = +this.route.snapshot.paramMap.get('id');
     this.techDetail = new TechnologyDetail();
     this.getTechnologyDetail();
   }
--- src/app/technologies/technologies.module.ts
+++ src/app/technologies/technologies.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TechnologiesListComponent } from './technologies-list/technologies-list.component';
 import { TechnologiesDetailComponent } from './technologies-detail/technologies-detail.component';
+import { TechnologiesCreateComponent } from './technologies-create/technologies-create.component';
 import { TechnologiesService } from './technologies.service';
 import { FormsModule } from '@angular/forms';
 import { MatTableModule, MatFormFieldModule,MatInputModule } from '@angular/material';
@@ -15,8 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         MatInputModule,
         BrowserAnimationsModule
     ],
-    declarations: [TechnologiesListComponent, TechnologiesDetailComponent],
+    declarations: [TechnologiesListComponent, TechnologiesDetailComponent, TechnologiesCreateComponent],
     providers: [TechnologiesService],
-    exports:[TechnologiesListComponent, TechnologiesDetailComponent]
+    exports:[TechnologiesListComponent, TechnologiesDetailComponent, TechnologiesCreateComponent]
 })
 export class TechnologiesModule {}
