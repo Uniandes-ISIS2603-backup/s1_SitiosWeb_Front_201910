cdbd4a38c3a92952ff6b0bb2dab9f2ca984bf60e
Allan <ar.corinaldi@uniandes.edu.co>
2019-05-20 20:44:26 -0500

    Sirve perfectamente create de changes

--- src/app/cambios/changes.module.ts
+++ src/app/cambios/changes.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ChangesCreateComponent } from './changes-create/changes-create.component';
 import { ChangesListComponent } from './changes-list/changes-list.component';
 import { ChangesService } from './changes.service';
-import { MatFormFieldModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+import { MatFormFieldModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatInputModule } from '@angular/material';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   imports: [
     CommonModule,
+    MatDatepickerModule,
+    MatInputModule,
     MatFormFieldModule,
     MatSelectModule,
     MatDatepickerModule,
