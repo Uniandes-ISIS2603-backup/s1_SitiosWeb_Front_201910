3bd1ceb9f08da3bf0be329e4a6e593cf644163ac
Allan <ar.corinaldi@uniandes.edu.co>
2019-05-21 23:02:16 -0500

    localhost works

--- src/app/appConstants.ts
+++ src/app/appConstants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
     public static get baseURL(): string {
         //IMPORTANT esta url se mantendra por los test y debe ser cambiada por localhost:8080 al momento de la entrega
-                return "http://c0024673.ngrok.io/s1_sitios-api/api/";//http://localhost:8080/s1_sitios-api/"http://655124c4.ngrok.io/s1_sitios-api/api/";
+                return "http://localhost:8080/s1_sitios-api/api/";//http://c0024673.ngrok.io/s1_sitios-api/api/"http://655124c4.ngrok.io/s1_sitios-api/api/";
     }
 }
